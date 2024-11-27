865db6a64a294fa3640fb6235e2959a116d4f50c
choi-eun-seo <sshses113@gmail.com>
2024-11-27 12:33:37 +0900

    Feat: add function for research time line

--- src/components/Composition/About/IntegrationStepModal.tsx
+++ src/components/Composition/About/IntegrationStepModal.tsx
@@ -1,13 +1,50 @@
-import { GradientBox } from "@/components/UI/Box/GradientBox";
 import { AssistiveStyle } from "@/components/UI/Text/AssistiveStyle";
 import { Text } from "@/components/UI/Text/Text";
-import { CustomImage } from "@/components/Utilities/Asset/CustomImage";
 import { Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/react";
 
 interface IntegrationStepModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
+
+const SECOND_LIST = [
+  {
+    id: "01",
+    content: "하드웨어 측정 기반 CPU 전력 예측 모델 개발",
+  },
+  { id: "02", content: "반복문 최적화 기법" },
+  { id: "03", content: "병렬 프로그래밍 모델 개발" },
+  { id: "04", content: "오토튜닝 메커니즘 개발" },
+];
+
+const THIRD_LIST = [
+  {
+    id: "01",
+    content: "하드웨어 측정 기반 CPU 전력 예측 모델 개발",
+  },
+  { id: "02", content: "반복문 최적화 기법" },
+  { id: "03", content: "병렬 프로그래밍 모델 개발" },
+];
+
+const RESEARCH_TIME_LINE = [
+  {
+    year: "2차년도",
+    summary:
+      "반복문 최적화 및 병렬 프로그래밍 모델 최적화와<br/> 오토튜닝 메커니즘 연계, 성능/전력 동시 최적화 비용 모델 개발",
+    list: SECOND_LIST,
+  },
+  {
+    year: "3차년도",
+    summary: "데이터 레이아웃 최적화와<br/> 오토튜닝 메커니즘 연계",
+    list: THIRD_LIST,
+  },
+  {
+    year: "4차년도",
+    summary:
+      "잡 스케줄링 최적화, 병렬 저장장치 시스템 최적화,<br/> 구현선택 최적화 기법과 오토튜닝 메커니즘 연계",
+    list: THIRD_LIST,
+  },
+];
 
 export const IntegrationStepModal: React.FC<IntegrationStepModalProps> = ({
   isOpen,
@@ -21,16 +58,77 @@ export const IntegrationStepModal: React.FC<IntegrationStepModalProps> = ({
       classNames={{
         base: "w-[1440px]",
       }}
-      className="min-w-[800px] max-w-[1440px] h-[1248px] max-h-[800px] min-h-[1248px] bg-white"
+      className="min-w-[1440px] max-w-[1440px] h-[1200px] max-h-[1200px] min-h-[1100px] bg-white"
     >
       <div>
         <ModalContent>
-          <ModalHeader></ModalHeader>
-          <ModalBody>
-            <Text>Integration Step Modal</Text>
+          <ModalHeader className="flex justify-center item-center  pt-[50px] pb-[48px]">
+            <AssistiveStyle
+              variant="h1"
+              className="text-[51px] font-normal leading-[61.20px]"
+            >
+              연구 통합 단계
+            </AssistiveStyle>
+          </ModalHeader>
+          <ModalBody className="px-[70px]">
+            {RESEARCH_TIME_LINE.map((item) =>
+              Divider(item.year, item.summary, item.list)
+            )}
           </ModalBody>
         </ModalContent>
       </div>
     </Modal>
   );
 };
+
+function Divider(
+  year: string,
+  summary: string,
+  list: { id: string; content: string }[]
+) {
+  return (
+    <>
+      <div className="border-dashed border-1 border-black" />
+      <div className="flex flex-col">
+        <div className="flex  flex-row">
+          <Text variant="b2" className="pl-[18px] !text-[24px] !font-bold">
+            {year}
+          </Text>
+          <Text
+            variant="h4"
+            className="text-[38px] !font-light px-[115px] tracking-tight"
+            dangerouslySetInnerHTML={{ __html: summary }}
+
+          />
+            {/* {summary}
+          </Text> */}
+        </div>
+        <div className="w-full box-border mt-[60px] pl-[211px] pr-[178px] mb-[88px]">
+          {list.map((item) => renderList(item.id, item.content))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function renderList(
+  id: string,
+  content: string,
+  highlightColor = "text-primary-strong"
+) {
+  return (
+    <div
+      key={id}
+      className="w-full h-16 bg-[#f2f2f2] rounded-[50px] mb-[12px] box-border flex items-center"
+    >
+      <Text
+        variant="h4"
+        className="text-[24px] font-normal font-['D2Coding'] leading-[33.6px] ml-[29px]"
+      >
+        {id} {`{`}
+        <span className={highlightColor}>{content}</span>
+        {`}`}
+      </Text>
+    </div>
+  );
+}
