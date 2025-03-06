d091f5f31fc47ee50f097628b78afdca12f9979e
윤선호 <prefer_youn@yunseonhoui-MacBookPro.local>
2025-03-06 11:40:46 +0900

    fix: font path

--- public/basePath.js
+++ public/basePath.js
@@ -1,1 +1,1 @@
-window.basePath = "/hyperf";
+window.basePath = "";
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 export const fetchFile = async <T = any>(url: string) => {
   const basePath = window?.["basePath" as any] ?? "";
   try {
-    return axios.get<T>(`${basePath}/hyperf${url}`);
+    return axios.get<T>(`${basePath}${url}`);
   } catch (e) {
     throw e;
   }
