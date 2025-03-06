edd2f7e1b415db99b48cec154313abc226a72555
윤선호 <prefer_youn@yunseonhoui-MacBookPro.local>
2025-03-06 11:09:19 +0900

    fix: setup

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   assetPrefix: isProduction ? "/hyperf" : "",
-
+  basePath: isProduction ? "/hyperf" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
